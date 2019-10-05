c7f29dcdef6dd5908718e93895868453be91e11d
Roman Paradeev <roman.paradeev@klarna.com>
2019-10-05 15:25:00 +0200

    feat: implement client

--- packages/client/lib/index.ts
+++ packages/client/lib/index.ts
@@ -1,5 +1,24 @@
-function client(a: number) {
-  console.log(a);
-}
+import ApolloClient, { gql } from "apollo-boost";
 
-client(1);
+const client = new ApolloClient({ uri: "http://localhost:5000" });
+
+const GET_USERS = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`;
+
+client.query({ query: GET_USERS }).then(res => {
+  const { users } = res.data;
+  const usersHtml = `
+    <h1>Users</h1>
+    <ul>
+      ${users.map(user => `<li>${user.name}</li>`)}
+    </ul>
+  `;
+
+  document.body.insertAdjacentHTML("afterend", usersHtml);
+});
